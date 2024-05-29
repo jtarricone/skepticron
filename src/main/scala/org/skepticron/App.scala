40fc72b16d7d67359f7d4dcad0a308826235d57a
Joe Tarricone <joe.tarricone@banno.com>
2024-05-29 13:48:44 -0400

    get file read working

--- src/main/scala/org/skepticron/App.scala
+++ src/main/scala/org/skepticron/App.scala
@@ -21,27 +21,35 @@ object App {
 
   }
 
-  case class Train(corpus: String, placeholder: String) // TODO
+  case class Train(corpus: String, placeholder: Option[String]) // TODO
   object Train {
     private val corpusOpts: Opts[String] =
       Opts.option[String]("corpus", "Filepath of the corpus to be used for training", "c")
 
-    private val placeholderOpts: Opts[String] =
-      Opts.option[String]("placeholder", "A placeholder for more interesting parameters to come", "p")
+    private val placeholderOpts: Opts[Option[String]] =
+      Opts.option[String]("placeholder", "A placeholder for more interesting parameters to come", "p").orNone
 
-    val opts = Opts.subcommand("train", "Train a model instance") {
-      (corpusOpts, placeholderOpts).mapN((_, _) => ())
+    val opts:Opts[Train] = Opts.subcommand("train", "Train a model instance") {
+      (corpusOpts, placeholderOpts).mapN(Train.apply)
     }
 
-    def run[F[_]: Async](c: String, p: String): F[Unit] = println("TODO").pure[F]
+    def run[F[_]: Async](train: Train): F[Unit] = {
+      val r = for {
+        _ <- Resource.eval(Sync[F].delay(println(s"Reading data from file at ${train.corpus}")))
+        trainingText <- Resource.eval(Utils.readFile[F](train.corpus))
+        _ <- Resource.eval(Sync[F].delay(println(s"Read ${trainingText.length} lines")))
+        _ <- Resource.eval(Sync[F].delay(println(trainingText).pure[F]))
+      } yield ()
+      r.use(_ => Sync[F].unit)
+    }
   }
 
   case class Generate(length: Long)
   object Generate {
     private val lengthOpts: Opts[Long] = Opts.option[Long]("length", "Length of generated output", "l")
 
-    val opts = Opts.subcommand("generate", "Generate output from a model instance") {
-      (lengthOpts).map(_ => ())
+    val opts: Opts[Generate] = Opts.subcommand("generate", "Generate output from a model instance") {
+      (lengthOpts).map(Generate.apply(_))
     }
 
     def run[F[_]: Async](length: Long): F[Unit] = println("TODO").pure[F]
--- src/main/scala/org/skepticron/Main.scala
+++ src/main/scala/org/skepticron/Main.scala
@@ -11,13 +11,14 @@ object Main extends CommandIOApp(
   version = "0.01"
 ) {
   def main: Opts[IO[ExitCode]] = {
-    (
+    val derp = (
       App.Train.opts orElse
       App.Generate.opts orElse
       App.About.opts orElse
       App.Mascot.opts
-    ).map{
-      case t: App.Train => App.Train.run[IO](t.corpus, t.placeholder).as(ExitCode.Success)
+    )
+      derp.map{
+      case t: App.Train => App.Train.run[IO](t).as(ExitCode.Success)
       case g: App.Generate => App.Generate.run[IO](g.length).as(ExitCode.Success)
       case _: App.About => App.About.run[IO]().as(ExitCode.Success)
       case _: App.Mascot => App.Mascot.run[IO]().as(ExitCode.Success)
